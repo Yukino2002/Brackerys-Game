bc25f2031ba1632d9d1ce3e11cb467bd678170f9
Pratik Jallan <74804069+Yukino2002@users.noreply.github.com>
2023-01-12 10:26:19 +0530

    Add collision feature to obstacles

---
+++ Brackeys/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public PlayerMovement movement;
+
+    // Update is called once per frame
+    void OnCollisionEnter (Collision collisionInfo) {
+        if (collisionInfo.collider.tag == "Obstacle") {
+            movement.enabled = false;
+        }
+    }
+}
