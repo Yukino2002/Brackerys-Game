22662dbe086915187fe28f0acce4bdac6b0f3be3
Pratik Jallan <74804069+Yukino2002@users.noreply.github.com>
2023-03-08 11:04:43 +0530

    Update readme

--- Brackeys/Assets/Scripts/PlayerMovement.cs
+++ Brackeys/Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
     public Rigidbody rb;
 
-    public float forwardForce = 1000f;
+    public float forwardForce = 800f;
     public float sidewaysForce = 600f;
 
     // FixedUpdate naming while manipulating physics, is called once per frame
@@ -12,11 +12,11 @@ public class PlayerMovement : MonoBehaviour {
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
         if (Input.GetKey("a")) {
-            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
         
         if (Input.GetKey("d")) {
-            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
     }
 }
